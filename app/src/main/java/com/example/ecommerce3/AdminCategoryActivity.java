32436eceb0d30d6fc82bc59ad2da583221460797
akAjikaran <ajikaran1997@gmail.com>
2021-04-26 10:38:18 +0530

    First Commit

---
+++ app/src/main/java/com/example/ecommerce3/AdminCategoryActivity.java
@@ -0,0 +1,143 @@
+package com.example.ecommerce3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class AdminCategoryActivity extends AppCompatActivity {
+
+    private ImageView leaf,food;
+    private ImageView fiber,tuber;
+    private ImageView fruit,sab;
+
+    private Button LogoutBtn, CheckOrdersBtn, maintainProductsBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_category);
+
+
+
+        LogoutBtn = (Button) findViewById(R.id.admin_logout_btn);
+        CheckOrdersBtn = (Button) findViewById(R.id.check_orders_btn);
+        maintainProductsBtn = (Button) findViewById(R.id.maintain_btn);
+
+
+        maintainProductsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, HomeActivity.class);
+                intent.putExtra("Admin", "Admin");
+                startActivity(intent);
+            }
+        });
+
+
+        LogoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        CheckOrdersBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminNewOrdersActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        leaf = (ImageView) findViewById(R.id.leaf_based_products);
+        food = (ImageView) findViewById(R.id.food_based_products);
+
+
+        fiber= (ImageView) findViewById(R.id.fiber_based_products);
+        tuber = (ImageView) findViewById(R.id.tuber_based_products);
+
+
+        fruit = (ImageView) findViewById(R.id.fruit_based_products);
+        sab = (ImageView) findViewById(R.id.sab_based_products);
+
+
+
+        leaf.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Leaf based products");
+                startActivity(intent);
+            }
+        });
+
+
+        food.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Food based products");
+                startActivity(intent);
+            }
+        });
+
+
+        fiber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Fiber based products");
+                startActivity(intent);
+            }
+        });
+
+
+        tuber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Tuber based products");
+                startActivity(intent);
+            }
+        });
+
+
+        fruit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Fruit based products");
+                startActivity(intent);
+            }
+        });
+
+
+
+        sab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(AdminCategoryActivity.this, AdminAddNewProductActivity.class);
+                intent.putExtra("category", "Sab based products");
+                startActivity(intent);
+            }
+        });
+
+    }
+}
